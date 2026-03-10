899bf1eadb3423af37a42c83d9d1e40eb1b19a9f
Manikanta <2400030217@kluniversity.in>
2026-03-10 11:50:00 +0530

    Added Hibernate Inventory Project

---
+++ src/main/java/com/klef/fsad/exam/ClientDemo.java
@@ -0,0 +1,44 @@
+package com.klef.fsad.exam;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Date;
+
+public class ClientDemo {
+
+public static void main(String[] args) {
+	java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.SEVERE);
+Configuration cfg = new Configuration();
+cfg.configure("hibernate.cfg.xml");
+
+SessionFactory sf = cfg.buildSessionFactory();
+Session session = sf.openSession();
+
+Transaction tx = session.beginTransaction();
+
+Inventory inv = new Inventory();
+inv.setName("Laptop");
+inv.setDescription("Dell Laptop");
+inv.setDate(new Date());
+inv.setStatus("Available");
+
+session.save(inv);
+
+tx.commit();
+
+System.out.println("Record Inserted Successfully");
+
+if(inv!=null)
+{
+session.delete(inv);
+System.out.println("Record Deleted");
+}
+session.close();
+sf.close();
+}
+
+
+}
